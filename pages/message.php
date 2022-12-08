a26716e4f5fac7e0dc42ebec7ccc2407755c3047
kabreaziz60 <kabreaziz60@gmail.com>
2022-12-08 00:44:30 +0000

    messagerie ajouter

--- pages/message.php
+++ pages/message.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Message</title>
-    <link rel="stylesheet" href="../Technologie-web/styles/message.css">
+    <link rel="stylesheet" href="../Technologie-web/styles/chat.css">
 </head>
 <body>
 <div id="container">
